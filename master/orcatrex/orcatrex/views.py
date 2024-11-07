d368074aaf94cf6c93f1838419bb2f5890911196
sagardhande <dhandesagar78@gmail.com>
2024-11-07 20:12:24 +0530

    updated view to apiview

--- master/orcatrex/orcatrex/views.py
+++ master/orcatrex/orcatrex/views.py
@@ -1,17 +1,14 @@
-from __future__ import annotations
-
 import threading
 from collections import deque
 
 from django.forms.models import model_to_dict
 from django.http import HttpResponse
-from django.utils.decorators import method_decorator
-from django.views import View
-from django.views.decorators.csrf import csrf_exempt
 from orcatrex.models import Jobs
 from orcatrex.models import Slave as ModelSlave
 from orcatrex.utils import (PriorityQueue, check_job_queue, copy_to_server, execute_jobs, get_best_slave, get_jobs_data,
                             get_slave_data, run_in_background)
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 SLAVE_DATA = get_slave_data()
 SLAVE_PQ = PriorityQueue()
@@ -23,43 +20,48 @@ job_queue_executor.daemon = True
 job_queue_executor.start()
 
 
-class GetJobs(View):
-  command: str
-  files: list[str]
+class GetJobs(APIView):
 
-  @method_decorator(csrf_exempt)
   def post(self, request):
-    self.command = request.POST.get("command")
-    self.dir_name = request.POST.get("dir")
+    command = request.data.get("command")
+    dir_name = request.data.get("dir")
+
     best_slave = get_best_slave(SLAVE_DATA, SLAVE_PQ)
-    job_data = Jobs(command=self.command, dir_name=self.dir_name)
+    job_data = Jobs(command=command, dir_name=dir_name)
     job_data.save()
+
     if not best_slave:
       JOBS_Q.append(job_data)
-      return HttpResponse(status=420)
+      return Response(status=420)
+
     SLAVE_DATA[best_slave]["number_of_existing_executions"] += 1
     execute_jobs(best_slave, model_to_dict(job_data))
-    job_update = Jobs.objects.get(id=job_data.id)
+
+    # Update job status to Running and then Completed
+    job_update = Jobs.objects.filter(id=job_data.id)
     job_update.update(status="Running")
     SLAVE_DATA[best_slave]["number_of_existing_executions"] -= 1
-    job_update = Jobs.objects.get(id=job_data.id)
     job_update.update(status="Completed")
-    return HttpResponse(status=200)
+
+    return Response(status=200)
 
 
-class SlaveAdder(View):
+class SlaveAdder(APIView):
 
-  # @Arsenalist Only Method
-  @method_decorator(csrf_exempt)
   def post(self, request):
     global SLAVE_DATA
-    username = request.POST.get("username")
-    hostname = request.POST.get("hostname")
-    active = request.POST.get("active", False)
-    is_gcloud = request.POST.get("is_gcloud", False)
-    if not username and not hostname:
-      return HttpResponse(content=b"Username/hostname not provided", status=420)
+
+    username = request.data.get("username")
+    hostname = request.data.get("hostname")
+    active = request.data.get("active", False)
+    is_gcloud = request.data.get("is_gcloud", False)
+
+    if not username or not hostname:
+      return Response(data={"error": "Username/hostname not provided"}, status=420)
+
     new_slave = ModelSlave(username=username, hostname=hostname, active=active, is_gcloud=is_gcloud)
     new_slave.save()
+
     SLAVE_DATA[new_slave.hostname] = model_to_dict(new_slave)
-    return HttpResponse(status=200)
+
+    return Response(status=200)
