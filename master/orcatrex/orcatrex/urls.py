8279b1ee31cb7d6fd06df14b7a6368a18e9a0725
sagardhande <dhandesagar78@gmail.com>
2024-11-07 17:56:46 +0000

    Minor fixes

--- master/orcatrex/orcatrex/urls.py
+++ master/orcatrex/orcatrex/urls.py
@@ -16,5 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from orcatrex import settings
+
 
 urlpatterns = [path('admin/', admin.site.urls), path('orcatrex/', include('orcatrex.orca_urls'))]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
