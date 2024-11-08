bcb34eccac9ebb57018dab2a91d6fdc959529542
sagardhande <dhandesagar78@gmail.com>
2024-11-08 13:03:32 +0530

    fixed issue for image id

--- master/orcatrex/orcatrex/gcloud_utils.py
+++ master/orcatrex/orcatrex/gcloud_utils.py
@@ -121,7 +121,7 @@ class DockerUtility:
     try:
       command = f'docker load -i {self.image}'
       output = self.server.run_command(command)
-      output = re.search(r"sha256:([a-f0-9]{64})", output)
+      output = re.search(r"sha256:([a-f0-9]{64})", output).group(1)
       self.image_id = output
       command = f'docker tag {output} trade-ai-image:latest'
       tag_output = self.server.run_command(command)
