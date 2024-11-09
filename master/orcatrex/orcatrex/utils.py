6bd00cdefcbaf4eb10ff1099744f2d66cba0dd3e
sagardhande <dhandesagar78@gmail.com>
2024-11-09 15:08:17 +0530

    fixed temp path

--- master/orcatrex/orcatrex/utils.py
+++ master/orcatrex/orcatrex/utils.py
@@ -111,7 +111,7 @@ def copy_to_server(slave, dir_name):
 
   for file in files_list:
     temp_index = file.split("/").index("temp")
-    file_dest = "/home/tradeai/temp" + "/".join(file.split("/")[temp_index + 1:])
+    file_dest = "/home/tradeai/temp/" + "/".join(file.split("/")[temp_index + 1:])
     server_obj.scp(file, file_dest)
 
 
