04ca5b9b58937c8a385a71210d1ad2fa12f5ccfe
sagardhande <dhandesagar78@gmail.com>
2024-11-09 15:21:13 +0530

    Minor fix

--- master/orcatrex/orcatrex/utils.py
+++ master/orcatrex/orcatrex/utils.py
@@ -94,7 +94,7 @@ def walk_dir(files_list, path):
   for root, dirs, files in os.walk(path):
     for file in files:
       full_file_path = os.path.join(root, file).split("/")
-      full_file_path.insert(2, "temp")
+      full_file_path.insert(3, "temp")
       full_file_path = "/".join(full_file_path)
       files_list.append(full_file_path)
     for dir_name in dirs:
