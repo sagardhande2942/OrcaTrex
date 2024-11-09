90a7830363d11dd86303a2a2495f9f2f5ddc3eeb
sagardhande <dhandesagar78@gmail.com>
2024-11-09 15:18:21 +0530

    Minor fix

--- master/orcatrex/orcatrex/utils.py
+++ master/orcatrex/orcatrex/utils.py
@@ -93,7 +93,9 @@ def slave_job_executor(slave, job_data):
 def walk_dir(files_list, path):
   for root, dirs, files in os.walk(path):
     for file in files:
-      full_file_path = "/".join(os.path.join(root, file).split("/").insert(2, "temp"))
+      full_file_path = os.path.join(root, file).split("/")
+      full_file_path.insert(2, "temp")
+      full_file_path = "/".join(full_file_path)
       files_list.append(full_file_path)
     for dir_name in dirs:
       walk_dir(files_list, f"{path}/{dir_name}")
